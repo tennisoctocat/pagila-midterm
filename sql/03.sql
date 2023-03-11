c9dd076eb59a9151d5096046dd84ff0041e4d116
Cynthia Hom <chom@g.hmc.edu>
2023-03-10 18:52:17 -0800

    finished first draft

--- sql/03.sql
+++ sql/03.sql
@@ -3,3 +3,21 @@
  * List the first and last names of all actors who have appeared in movies in the "Children" category,
  * but that have never appeared in movies in the "Horror" category.
  */
+
+/* This query assumes that by "appeared in movies" we mean "has appeared in one or more movies", not "has appeared in 2 or more movies". */
+SELECT DISTINCT first_name, last_name FROM actor
+    JOIN film_actor USING (actor_id)
+    JOIN film USING (film_id)
+    JOIN film_category USING (film_id)
+    JOIN category USING (category_id)
+    WHERE name='Children'
+    EXCEPT ALL
+    (
+        SELECT first_name, last_name FROM actor
+            JOIN film_actor USING (actor_id)
+            JOIN film USING (film_id)
+            JOIN film_category USING (film_id)
+            JOIN category USING (category_id)
+            WHERE name='Horror'
+    )
+    ORDER BY last_name ASC, first_name ASC;
--- sql/04.sql
+++ sql/04.sql
@@ -5,3 +5,12 @@
  * Lists the title of all movies where at least 2 actors were also in 'AMERICAN CIRCUS'.
  * (You may choose to either include or exclude the movie 'AMERICAN CIRCUS' in the results.)
  */
+
+/* We include 'AMERICAN CIRCUS' in the results */
+SELECT film_b.title FROM film_actor film_actor_a
+        JOIN film_actor film_actor_b USING (actor_id)
+        JOIN film film_a ON (film_actor_a.film_id = film_a.film_id)
+        JOIN film film_b ON (film_actor_b.film_id = film_b.film_id)
+        WHERE film_a.title = 'AMERICAN CIRCUS'
+        GROUP BY film_b.title
+        HAVING count(actor_id) >= 2 ORDER BY film_b.title;
